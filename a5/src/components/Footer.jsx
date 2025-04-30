0f9add12cc6a4dc569b535503bacf10ac541d13a
Jennifer D <[email]jennifer.duan2@student.tdsb.on.ca>
2025-04-30 00:19:16 -0400

    added header, footer, feature, and hero component

---
+++ a5/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="footer-grid">
+                <a href="i">Account</a>
+                <a href="h">Media Notice</a>
+            </div>
+            <div className="footer-grid">
+                <a href="g">Terms of Service</a>
+                <a href="f">Privacy Notice</a>
+            </div>
+            <div className="footer-grid">
+                <a href="e"></a>
+                <a href="d">Redeem</a>
+                <a href="c">New Deals</a>
+            </div>
+            <div className="footer-grid">
+                <a href="b">FAQ</a>
+                <a href="a">© Meow Movies!</a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ a5/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import "./Header.css";
+import MeowMovies from "../assets/images/weblogo.png";
+import Search from "../assets/images/searching.png";
+import { useNavigate } from "react-router";
+
+function Header() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="header">
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400..700;1,400..700&family=Staatliches&display=swap" rel="stylesheet">
+            </link>
+            
+            <div>
+                <img src={MeowMovies} alt="MeowMovies logo" width="50" id="logo" />
+                <div className="header-text"  onClick={() => navigate(`/`)}>
+                    <div className="webtitle">MEOW MOVIES</div>
+                </div>
+            </div>
+            <div className="header-frame">
+                <input type="text" id="search-bar" height="30" placeholder="Search" />
+                <button id="search-button">
+                    <img src={Search} alt="search button" height="30" />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
